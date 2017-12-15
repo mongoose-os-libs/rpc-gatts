e90c3c0145afe972a6d02c2e4bceab4e1f838a95
Deomid Ryabkov <rojer@cesanta.com>
2017-12-15 17:21:27 +0300

    Implement mg_rpc_get_channel_info()
    Also rename `struct mg_rpc_authn` to `struct mg_rpc_authn_info`, for consistency.
    
    PUBLISHED_FROM=41d7eb0a2b56c13dbe947bd92a7e2d0ba4afff50

--- src/esp32/esp32_rpc_channel_gatts.c
+++ src/esp32/esp32_rpc_channel_gatts.c
@@ -181,10 +181,9 @@ static const char *mg_rpc_ch_gatts_get_type(struct mg_rpc_channel *ch) {
   return "GATTS";
 }
 
-static bool mg_rpc_channel_gatts_get_authn_info(struct mg_rpc_channel *ch,
-                                                const char *auth_domain,
-                                                const char *auth_file,
-                                                struct mg_rpc_authn *authn) {
+static bool mg_rpc_channel_gatts_get_authn_info(
+    struct mg_rpc_channel *ch, const char *auth_domain, const char *auth_file,
+    struct mg_rpc_authn_info *authn) {
   (void) ch;
   (void) auth_domain;
   (void) auth_file;
